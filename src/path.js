2d93d21a5182350950d6a7bb4903bc44846d4535
icodelifee <mithunsmenon@gmail.com>
2020-05-19 14:03:05 +0530

    Store all paths in single file

---
+++ src/path.js
@@ -0,0 +1,8 @@
+module.exports = {
+    kModule: '/sys/devices/platform/faustus/',
+	fanModeBoost: '/sys/devices/platform/faustus/fan_boost_mode',
+	fanModeTTP: '/sys/devices/platform/faustus/throttle_thermal_policy',
+	brightness: '/sys/devices/platform/faustus/leds/asus::kbd_backlight/brightness',
+    rgb: '/sys/devices/platform/faustus/kbbl/*',
+    kblMode: '/sys/devices/platform/faustus/kbbl/kbbl_mode'
+};
